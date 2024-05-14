6f15c4c24f24a516c9ab29efbcb6e036e575c6ee
Dominik Markart <domme190800@gmail.com>
2024-05-14 16:05:53 +0200

    Update migrations

--- build/prisma/migrations/20240513152258_add_last_tenant_name_to_user_table/migration.sql
+++ build/prisma/migrations/20240513152258_add_last_tenant_name_to_user_table/migration.sql
@@ -4,14 +4,6 @@
   - You are about to drop the `_UserTenant` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE "_UserTenant" DROP CONSTRAINT "_UserTenant_A_fkey";
-
--- DropForeignKey
-ALTER TABLE "_UserTenant" DROP CONSTRAINT "_UserTenant_B_fkey";
 
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "lastTenantName" TEXT;
-
--- DropTable
-DROP TABLE "_UserTenant";
--- src/prisma/migrations/20240408122430_base/migration.sql
+++ src/prisma/migrations/20240408122430_base/migration.sql
@@ -25,11 +25,7 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "_UserTenant" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL
-);
+
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Tenant_name_key" ON "Tenant"("name");
@@ -40,14 +36,3 @@ CREATE UNIQUE INDEX "Tenant_url_key" ON "Tenant"("url");
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- CreateIndex
-CREATE UNIQUE INDEX "_UserTenant_AB_unique" ON "_UserTenant"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_UserTenant_B_index" ON "_UserTenant"("B");
-
--- AddForeignKey
-ALTER TABLE "_UserTenant" ADD CONSTRAINT "_UserTenant_A_fkey" FOREIGN KEY ("A") REFERENCES "Tenant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_UserTenant" ADD CONSTRAINT "_UserTenant_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
